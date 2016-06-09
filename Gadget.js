3bca45740e5fdf1f6fd53dbcde1e57de6e4daba9
takaaki koike <taka.vao.321@gmail.com>
2016-06-09 23:06:28 +0900

    collectionも

--- Gadget.js
+++ Gadget.js
@@ -2,53 +2,60 @@ var Gadget = function(name, category, price){
   this.name = name !== undefined ? name : "no name";
   this.category = category !== undefined ? category : "others";
   this.price = price !== undefined ? price : 0;
-}
+};
 
 Gadget.prototype.name = "no name";
 Gadget.prototype.category = "others";
 Gadget.prototype.price = 0;
 Gadget.prototype.specs = {};
 
-Gadget.getName = function(){
+Gadget.prototype.getName = function(){
   return this.name;
-}
+};
 
-Gadget.setName = function(name){
+Gadget.prototype.setName = function(name){
   this.name = name;
-}
+};
 
-Gadget.getCategory = function(){
+Gadget.prototype.getCategory = function(){
   return this.category;
-}
+};
 
-Gadget.setCategory = function(category){
+Gadget.prototype.setCategory = function(category){
   this.category = category;
-}
+};
 
-Gadget.getPrice = function(){
+Gadget.prototype.getPrice = function(){
   return this.price;
-}
+};
 
-Gadget.setPrice = function(price){
+Gadget.prototype.setPrice = function(price){
   this.price = price;
-}
+};
 
-Gadget.clearSpecs = function(){
+Gadget.prototype.clearSpecs = function(){
   this.specs = {};
-}
+};
 
-Gadget.setSpec = function(l0, v0){
+Gadget.prototype.setSpec = function(l0, v0){
   this.specs[l0] = v0;
-}
+};
 
-Gadget.getSpec = function(l0){
+Gadget.prototype.getSpec = function(l0){
   return this.specs[l0];
-}
+};
 
-Gadget.getSpecs = function(){
+Gadget.prototype.getSpecs = function(){
   var list = [];
   for(var p in this.specs){
     list.push({label:p, value:this.specs[p]});
   }
   return list;
+};
+
+Gadget.prototype.getSpecString = function(){
+  var str = "";
+  for(var p in this.specs){
+    str += this.specs[p] + ";";
+  }
 }
