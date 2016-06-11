233baea6ac9d1d70efbcac8e7fcd9068f74ab5b3
kioke takaaki <taka.vao.321@gmail.com>
2016-06-11 16:28:33 +0900

    edit機能を有効化

--- wishList/script.js
+++ wishList/script.js
@@ -12,10 +12,22 @@ $(".itemDialog").on('show.bs.modal', function(event){
   var applyBtn = $(".itemFormApplyBtn");
   var command = btn.data("command");
   var index = btn.data("index");
+  var form_name = $("#itemForm-ItemName");
+  var form_price = $("#itemForm-ItemPrice");
+
   if( command == "add" ){
-    applyBtn.data("command", command);
-    $("#itemForm-ItemName").val("");
-    $("#itemForm-ItemPrice").val("");
+    applyBtn.data("command", "add");
+    form_name.val("");
+    form_price.val("");
+
+  }else if( command == "edit" ){
+    if( index >= 0 && index < ITEMLIST.length() ){
+      applyBtn.data("command", "edit");
+      applyBtn.data("index", index);
+      form_name.val(ITEMLIST.get(index).getName());
+      form_price.val(ITEMLIST.get(index).getPrice());
+    }
+
   }else{
     console.log("undefined command:" + command);
   }
@@ -26,8 +38,17 @@ $(".itemFormApplyBtn").on('click', function(event){
   var iprice = $("#itemForm-ItemPrice").val();
   var com = $(this).data("command");
   $(this).data("command", null);
+  var idx = $(this).data("index");
+  $(this).data("index", null);
+
   if( com == "add" ){
     ITEMLIST.add(new Item(iname, iprice));
+
+  }else if( com == "edit" ){
+    if( idx >= 0 && idx < ITEMLIST.length() ){
+      ITEMLIST.set(idx, new Item(iname, iprice));
+    }
+
   }else{
     console.log("undefined command: " + com);
   }
@@ -44,7 +65,8 @@ function setItemlistToTable(table, list){
       $("<tr></tr>").append(
         $("<th></th>").text("id"),
         $("<th></th>").text("名前"),
-        $("<th></th>").text("価格")
+        $("<th></th>").text("価格"),
+        $("<th></th>").text("操作")
       )
     )
   );
@@ -55,7 +77,17 @@ function setItemlistToTable(table, list){
       $("<tr></tr>").append(
         $("<td></td>").text(i),
         $("<td></td>").text(item.getName()),
-        $("<td></td>").text(item.getPrice())
+        $("<td></td>").text(item.getPrice()),
+        $("<td></td>").append(
+          $("<button></button>")
+          .attr("data-toggle", "modal")
+          .attr("data-target", "#myModal")
+          .attr("data-command", "edit")
+          .attr("data-index", i).append(
+            $("<span></span>").attr("class", "glyphicon glyphicon-pencil"),
+            "編集"
+          ).attr("class", "btn btn-default")
+        )
       )
     );
   }
@@ -63,7 +95,8 @@ function setItemlistToTable(table, list){
     $("<tr></tr>").append(
       $("<td></td>").attr("colspan", "2")
         .text("合計"),
-      $("<td></td>").text(list.getSumPrice())
+      $("<td></td>").text(list.getSumPrice()),
+      $("<td></td>")
     )
   );
   table.append(tbody);
